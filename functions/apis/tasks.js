474c05af709736f61e18d3ef77700a0a84d5864a
Drew-Klepfer <klepfed@wwu.edu>
2023-06-19 16:03:41 -0700

    delete api working

--- functions/apis/tasks.js
+++ functions/apis/tasks.js
@@ -10,7 +10,7 @@ exports.getAllTasks = (request, response) => {
 			let tasks = [];
 			data.forEach((doc) => {
 				tasks.push({
-                    todoId: doc.id,
+                    taskId: doc.id,
                     title: doc.data().title,
 					description: doc.data().description,
 					status: doc.data().status,
@@ -27,8 +27,8 @@ exports.getAllTasks = (request, response) => {
 		});
 };
 
+/* add a task */
 exports.postOneTask = (request, response) => {
-	console.log(request.body.description.trim(), request.body.title.trim(), request.body.status.trim())
 	/* empty input check */
 	if (request.body.description.trim() === '') {
 		return response.status(400).json({ description: 'Must not be empty' });
@@ -41,12 +41,16 @@ exports.postOneTask = (request, response) => {
 	if(request.body.status.trim() === '') {
         return response.status(400).json({ status: 'Must not be empty' });
     }
+
+	if(request.body.dueDate.trim() === '') {
+        return response.status(400).json({ dueDate: 'Must not be empty' });
+    }
     
     const newTaskItem = {
         title: request.body.title,
         description: request.body.description,
         createdAt: new Date().toISOString(),
-		//dueDate: request.body.dueDate,
+		dueDate: request.body.dueDate,
 		status: request.body.status
     }
 	/* add new task to tasks firestore collection */
@@ -62,4 +66,27 @@ exports.postOneTask = (request, response) => {
 			response.status(500).json({ error: 'Something went wrong' });
 			console.error(err);
 		});
+};
+
+/* delete a task */
+exports.deleteTask = (request, response) => {
+	console.log(request.params.taskId);
+    const document = db.doc(`/tasks/${request.params.taskId}`);
+	console.log(document);
+    document
+        .get()
+        .then((doc) => {
+			//console.log(doc);
+            if (!doc.exists) {
+                return response.status(404).json({ error: 'Task not found' })
+            }
+            return document.delete();
+        })
+        .then(() => {
+            response.json({ message: 'Delete successful' });
+        })
+        .catch((err) => {
+            //console.error(err);
+            return response.status(500).json({ error: err.code });
+        });
 };
--- functions/index.js
+++ functions/index.js
@@ -3,11 +3,13 @@ const app = require('express')();
 
 const {
     getAllTasks,
-    postOneTask
+    postOneTask,
+    deleteTask
 } = require('./apis/tasks')
 
 app.get('/tasks', getAllTasks);
 app.post('/task', postOneTask);
+app.delete('/task/:taskId', deleteTask);
 
 
 
